1d01d58c2a7778d39f124b1168c7f7a0fab71d0b
SimonBucko <bucino.36@gmail.com>
2021-12-02 00:04:32 +0100

    added changes

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,35 +1,21 @@
-if (!document.getElementById) {
+import { SERVER_URL } from "../../constants.js";
 
-    // FONTS
-    document.write('<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600%7CUbuntu:300,400,500,700">');
-
-    //CSS
-    document.write('<link rel="stylesheet" type="text/css" href="/resources/css/main.css">');
-    document.write('<link rel="stylesheet" type="text/css" href="/resources/css/bootstrap-reboot.min.css">');
-    document.write('<link rel="stylesheet" type="text/css" href="/resources/css/bootstrap-grid.min.css">');
-    document.write('<link rel="stylesheet" type="text/css" href="/resources/css/owl.carousel.min.css">');
-    document.write('<link rel="stylesheet" type="text/css" href="/resources/css/jquery.mCustomScrollbar.min.css">');
-    document.write('<link rel="stylesheet" type="text/css" href="/resources/css/nouislider.min.css">');
-    document.write('<link rel="stylesheet" type="text/css" href="/resources/css/ionicons.min.css">');
-    document.write('<link rel="stylesheet" type="text/css" href="/resources/css/plyr.css">');
-    document.write('<link rel="stylesheet" type="text/css" href="/resources/css/photoswipe.css">');
-    document.write('<link rel="stylesheet" type="text/css" href="/resources/css/default-skin.css">');
-
-
-    //Favicons
-    document.write('<link rel="icon" type="image/png" href="/resources/icon/favicon-32x32.png" sizes="32x32">');
-    document.write('<link rel="apple-touch-icon" href="/resources/icon/favicon-32x32.png">');
-    document.write('<link rel="apple-touch-icon" href="/resources/icon/apple-touch-icon-72x72.png" sizes="72x72">');
-    document.write('<link rel="apple-touch-icon" href="/resources/icon/apple-touch-icon-114x114.png" sizes="114x114">');
-    document.write('<link rel="apple-touch-icon" href="/resources/icon/apple-touch-icon-144x144.png" sizes="144x144">');
+const fetchData = async () => {
+    try {
+        const response = await fetch(`${SERVER_URL}/api/playing?theaterId=1`);
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-export default () => {
+const getHTML = async () => {
     const content = document.querySelector("#content");
-
-    return fetch("./pages/main/main.html")
-        .then((response) => response.text())
-        .then((theHtml) => {
-            content.innerHTML = theHtml;
-        })
+    const response = await fetch("./pages/main/main.html")
+    const html = await response.text()
+    content.innerHTML = html;
+    // fetchData()
 }
+
+export default () => getHTML()
