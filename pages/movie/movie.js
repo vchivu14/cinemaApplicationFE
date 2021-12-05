e4695ccd82e41cbed51e0ab3c5110dd51c3b423e
SimonBucko <bucino.36@gmail.com>
2021-12-05 19:21:25 +0100

    added dynamic display of the movie page

--- pages/movie/movie.js
+++ pages/movie/movie.js
@@ -5,9 +5,8 @@ const fetchData = async (movieId) => {
     try {
         const response = await fetch(`${SERVER_URL}/api/movies/${movieId}`);
         const data = await response.json()
-        // const listOfMoviesSection = document.querySelector("#listOfMovies");
-        generateHtml(listOfMoviesSection, data)
-
+        const videopage = document.querySelector("#videopage");
+        generateHtml(videopage, data)
     } catch (error) {
         console.log(error)
     }
@@ -24,7 +23,38 @@ const getHTML = async (params) => {
 export default (params) => getHTML(params)
 
 const generateHtml = (parentElement, movie) => {
-    let HTML = ``;
+    let HTML = `
+    <section class="videopage">
+        <div class="mainIMG">
+            <img src="${movie.image}" alt=${movie.title}">
+        </div>
+        <div class="title_movie">
+            <h5>${movie.title}</h5>
+            <br>
+            <ul>
+                <li>
+                    <p>Min age: ${movie.minAge}</p>
+                </li>
+                <li>
+                    <p>Category: ${movie.category}</p>
+                </li>
+                <li>
+                    <p>Rating: ${movie.rating}/10</p>
+                </li>
+                <li>
+                    <p>${movie.actorList.map(actor => `${actor.firstName} ${actor.lastName}`)}</p>
+                </li>
+            </ul>
+            <br>
+            <h4>Description:</h4>
+            <br>
+            <p class="desc">${movie.description}</p>
+        </div>
+        <div class="videoPage">
+            <iframe class="iframe" src="${movie.trailer}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </section>
+    `;
     // loader.style.display = 'none';
     parentElement.innerHTML = HTML;
 }
