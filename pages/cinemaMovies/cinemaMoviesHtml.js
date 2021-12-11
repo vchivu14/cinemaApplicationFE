8ba17b120d0b3e1bce96c219d63ea5dbcd8dcdbf
SimonBucko <bucino.36@gmail.com>
2021-12-11 20:51:20 +0100

    updating is finished

--- pages/cinemaMovies/cinemaMoviesHtml.js
+++ pages/cinemaMovies/cinemaMoviesHtml.js
@@ -13,6 +13,7 @@ const fetchInitialData = async () => {
         addListenerForRows();
         addListenerToCloseUpdateModal();
         addListenerForCreateMovieForm();
+        addListenerForUpdateMovieForm();
     } catch (error) {
         console.log(error)
     }
@@ -68,6 +69,44 @@ const addListenerForCreateMovieForm = () => {
                 //hide modal and display button
                 document.getElementById("firstContainerChildOne").style.display = "none"
                 document.getElementById("firstContainerChildOneButtonShow").style.display = "block"
+            })
+            .catch(error => console.log(error));
+    })
+}
+
+const addListenerForUpdateMovieForm = () => {
+    const form = document.querySelector("#movieUpdateForm");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault()
+        const data = {
+            id: form.movieId.value,
+            actorList: JSON.parse(form.actors.value),
+            categoryId: form.category.value,
+            description: form.description.value,
+            image: form.image.value,
+            minAge: form.minAge.value,
+            rating: form.rating.value,
+            title: form.title.value,
+            trailer: form.trailer.value,
+        }
+        //sending actor list needs to be in JSON as input. Example below
+        //[{"firstName": "S", "lastName":"B"}] 
+
+        fetch(`${SERVER_URL}/api/movies?replace=true`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then(response => response.json())
+            .then(data => {
+                //easy and primitive way how we can refresh html table
+                fetchInitialData()
+                //reset form
+                form.reset()
+                //hide modal and display button
+                document.getElementById("updateMovieModal").style.display = "none"
             })
             .catch(error => console.log(error));
     })
