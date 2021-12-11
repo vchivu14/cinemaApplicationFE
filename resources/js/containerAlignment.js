69723f9a596bf4ede7c7d86c1c7d3c2ad6aa0c8b
SimonBucko <bucino.36@gmail.com>
2021-12-11 14:10:32 +0100

    added updating modal

--- resources/js/containerAlignment.js
+++ resources/js/containerAlignment.js
@@ -1,21 +1,21 @@
 function waitForElement() {
     if (document.getElementById("firstContainerChildOne") !== null) {
         function functionShowFirstChildContainerOne() {
-            document.querySelector("#firstContainerChildOne").style.visibility = 'visible';
-            document.querySelector("#firstContainerChildOneButtonShow").style.visibility = 'hidden';
-            document.querySelector("#firstContainerChildOneButtonUnShow").style.visibility = 'visible';
+            document.querySelector("#firstContainerChildOne").style.display = 'block';
+            document.querySelector("#firstContainerChildOneButtonShow").style.display = 'none';
+            document.querySelector("#firstContainerChildOneButtonUnShow").style.display = 'block';
         }
         let firstContainerChildOneButtonShow = document.querySelector("#firstContainerChildOneButtonShow");
         firstContainerChildOneButtonShow.addEventListener("click", functionShowFirstChildContainerOne);
-        
+
         function functionUnShowFirstChildContainerOne() {
-            document.querySelector("#firstContainerChildOne").style.visibility = 'hidden';
-            document.querySelector("#firstContainerChildOneButtonShow").style.visibility = 'visible';
-            document.querySelector("#firstContainerChildOneButtonUnShow").style.visibility = 'hidden';
+            document.querySelector("#firstContainerChildOne").style.display = 'none';
+            document.querySelector("#firstContainerChildOneButtonShow").style.display = 'block';
+            document.querySelector("#firstContainerChildOneButtonUnShow").style.display = 'none';
         }
         let firstContainerChildOneButtonUnShow = document.querySelector("#firstContainerChildOneButtonUnShow");
         firstContainerChildOneButtonUnShow.addEventListener("click", functionUnShowFirstChildContainerOne);
-    
+
     } else {
         setTimeout(waitForElement, 250);
     }
